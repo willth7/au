9871cfedbc28752d232afd4a17317ccd3c9f546d
Will Thomas <will@willth.dev>
2022-08-11 12:41:47 -0400

    symbols and relocations

--- src/au.c
+++ src/au.c
@@ -20,12 +20,6 @@
 #include "avr/avr.h"
 #include "elf/elf.h"
 
-typedef struct sym_s {
-	int8_t* sym;
-	uint16_t off;
-	uint16_t typ;
-} sym_t;
-
 int8_t main(int32_t argc, int8_t** argv) {
 	if (argc != 3) {
 		printf("error: too few arguments\n");
@@ -45,18 +39,21 @@ int8_t main(int32_t argc, int8_t** argv) {
 	fclose(f);
 	
 	uint8_t bits[65536];
-	uint16_t bi = 0;
+	uint16_t bn = 0;
 	int8_t e = 0;
 	
-	sym_t sym[65536];
-	uint16_t si = 0;
+	int8_t str[65536];
+	uint16_t strn = 0;;
 	
-	sym_t rel[65536];
-	uint16_t ri = 0;
+	elf_st32_t sym[256];
+	uint8_t symn = 0;
 	
-	int8_t str[3][256];
-	uint8_t sx = 0;
-	uint8_t sy = 0;
+	elf_r32_t rel[256];
+	uint8_t reln = 0;
+	
+	int8_t lex[3][256];
+	uint8_t x = 0;
+	uint8_t y = 0;
 	int8_t com = 0;
 	uint16_t ln = 0;
 	
@@ -66,81 +63,154 @@ int8_t main(int32_t argc, int8_t** argv) {
 		}
 		
 		if (data[fi] != ' ' && data[fi] != '\t' && data[fi] != '\n' && data[fi] != ';' && data[fi] != ',' && data[fi] != ':' && !com) {
-			str[sx][sy] = data[fi];
-			sy++;
-			str[sx][sy] = 0;
+			lex[x][y] = data[fi];
+			y++;
+			lex[x][y] = 0;
 		}
-		else if ((data[fi] == ' ' || data[fi] == '\t') && sx == 0 && sy && !com) {
-			sx = 1;
-			sy = 0;
+		else if ((data[fi] == ' ' || data[fi] == '\t') && x == 0 && y && !com) {
+			x = 1;
+			y = 0;
 		}
-		else if (data[fi] == ',' && sx == 1 && sy && !com) {
-			sx = 2;
-			sy = 0;
+		else if (data[fi] == ',' && x == 1 && y && !com) {
+			x = 2;
+			y = 0;
 		}
-		else if (data[fi] == ':' && sx == 0 && !com) {
-			sym[si].sym = malloc(strlen(str[0]) + 1);
-			strcpy(sym[si].sym, str[0]);
-			sym[si].off = bi;
-			sym[si].typ = 1;
-			si++;
-			str[0][0] = 0;
-			sy = 0;
+		else if (data[fi] == ':' && x == 0 && !com) {
+			uint32_t stri = elf_loct(str, strn, lex[0], strlen(lex[0]) + 1);
+			if (stri == 4294967295) {
+				stri = strn;
+				strn = elf_copy(str, strn, lex[0], strlen(lex[0]) + 1);
+				
+			}
+			
+			uint8_t symi = 255;
+			for (uint8_t i = 0; i < symn; i++) {
+				if (sym[i].name == stri) {
+					sym[i].value = bn;
+					symi = i;
+					break;
+				}
+			}
+			if (symi >= symn) {
+				sym[symn].name = stri;
+				sym[symn].value = bn;
+				sym[symn].size = 0;
+				sym[symn].info = 0;
+				sym[symn].other = 0;
+				sym[symn].shndx = 0;
+				
+				symi = symn;
+				symn++;
+			}
+			
+			lex[0][0] = 0;
+			y = 0;
 		}
 		else if (data[fi] == ';' && !com) {
 			com = 1;
-			sy = 0;
+			y = 0;
 		}
-		else if (data[fi] == '\n' && sx) {
+		else if (data[fi] == '\n' && x) {
 			com = 0;
-			printf("%s %s, %s\n", str[0], str[1], str[2]);
-			sx = 0;
-			sy = 0;
+			printf("%s %s, %s\n", lex[0], lex[1], lex[2]);
+			x = 0;
+			y = 0;
 			
 			enc_t enc;
 			err_t err;
 			err.b = 0;
-			avr_t avr = avr_enc(str[0], &err);
+			avr_t avr = avr_enc(lex[0], &err);
 			int8_t rb = 0;
 			
 			uint16_t rd;
-			if (avr.rd && str[1][0]) {
-				rd = avr.rd(str[1], &err, &rb);
+			if (avr.rd && lex[1][0]) {
+				rd = avr.rd(lex[1], &err, &rb);
 			}
-			else if (avr.rd && !str[1][0]) {
+			else if (avr.rd && !lex[1][0]) {
 				strcpy(err.e, "too few arguments");
 				err.b = 1;
 			}
-			else if (str[1][0]) {
+			else if (lex[1][0]) {
 				strcpy(err.e, "too many arguments");
 				err.b = 1;
 			}
 			if (rb && !err.b) {
-				rel[ri].sym = malloc(strlen(str[1]) + 1);
-				strcpy(rel[ri].sym, str[1]);
-				rel[ri].off = bi;
-				rel[ri].typ = avr.typ;
-				ri++;
+				uint32_t stri = elf_loct(str, strn, lex[1], strlen(lex[1]) + 1);
+				if (stri == 4294967295) {
+					stri = strn;
+					strn = elf_copy(str, strn, lex[1], strlen(lex[1]) + 1);
+					
+				}
+				
+				uint8_t symi = 255;
+				for (uint8_t i = 0; i < symn; i++) {
+					if (sym[i].name == stri) {
+						symi = i;
+						break;
+					}
+				}
+				if (symi >= symn) {
+					sym[symn].name = stri;
+					sym[symn].value = 0;
+					sym[symn].size = 0;
+					sym[symn].info = 16;
+					sym[symn].other = 0;
+					sym[symn].shndx = 0;
+					
+					symi = symn;
+					symn++;
+				}
+				
+				rel[reln].offset = bn;
+				rel[reln].info = (avr.typ & 255) | (symi << 8);
+				reln++;
+				
 				rb = 0;
 			}
 			
 			uint16_t rs;
-			if (avr.rs && str[2][0]) {
-				rs = avr.rs(str[2], &err, &rb);
+			if (avr.rs && lex[2][0]) {
+				rs = avr.rs(lex[2], &err, &rb);
 			}
-			else if (avr.rs && !str[2][0]) {
+			else if (avr.rs && !lex[2][0]) {
 				strcpy(err.e, "too few arguments");
 				err.b = 1;
 			}
-			else if (str[2][0]) {
+			else if (lex[2][0]) {
 				strcpy(err.e, "too many arguments");
 				err.b = 1;
 			}
 			if (rb && !err.b) {
-				rel[ri].sym = malloc(strlen(str[1]) + 1);
-				strcpy(rel[ri].sym, str[1]);
-				rel[ri].off = bi;
-				ri++;
+				uint32_t stri = elf_loct(str, strn, lex[2], strlen(lex[2]) + 1);
+				if (stri == 4294967295) {
+					stri = strn;
+					strn = elf_copy(str, strn, lex[2], strlen(lex[2]) + 1);
+					
+				}
+				
+				uint8_t symi = 255;
+				for (uint8_t i = 0; i < symn; i++) {
+					if (sym[i].name == stri) {
+						symi = i;
+						break;
+					}
+				}
+				if (symi >= symn) {
+					sym[symn].name = stri;
+					sym[symn].value = 0;
+					sym[symn].size = 0;
+					sym[symn].info = 16;
+					sym[symn].other = 0;
+					sym[symn].shndx = 0;
+					
+					symi = symn;
+					symn++;
+				}
+				
+				rel[reln].offset = bn;
+				rel[reln].info = (avr.typ & 255) | (symi << 8);
+				reln++;
+				
 				rb = 0;
 			}
 			
@@ -149,14 +219,14 @@ int8_t main(int32_t argc, int8_t** argv) {
 			}
 			
 			for (uint8_t ei = 0; ei < enc.n; ei++) {
-				if (bi % 2 == 0) {
+				if (bn % 2 == 0) {
 					printf("0x");
 					
 				}
-				bits[bi] = enc.x[ei];
-				printf("%02x", bits[bi]);
-				bi++;
-				if (bi % 2 == 0) {
+				bits[bn] = enc.x[ei];
+				printf("%02x", bits[bn]);
+				bn++;
+				if (bn % 2 == 0) {
 					printf("\n");
 					
 				}
@@ -166,19 +236,19 @@ int8_t main(int32_t argc, int8_t** argv) {
 				printf("error: %s\n", err.e);
 			}
 			
-			str[0][0] = 0;
-			str[1][0] = 0;
-			str[2][0] = 0;
+			lex[0][0] = 0;
+			lex[1][0] = 0;
+			lex[2][0] = 0;
 		}
 	}
 	free(data);
-	for (uint16_t i = 0; i < si; i++) {
-		printf("symbol: %s\noffset: %i\ntype: %i\n", sym[i].sym, sym[i].off, sym[i].typ);
-		free(sym[i].sym);
+	printf("\n");
+	for (uint16_t i = 0; i < symn; i++) {
+		printf("symbol: %s\noffset: %i\ninfo: %i\n", str + sym[i].name, sym[i].value, sym[i].info);
 	}
-	for (uint16_t i = 0; i < ri; i++) {
-		printf("rel symbol: %s\noffset: %i\ntype: %i\n", rel[i].sym, rel[i].off, rel[i].typ);
-		free(rel[i].sym);
+	printf("\n");
+	for (uint16_t i = 0; i < reln; i++) {
+		printf("rel: %s\noffset: %i\ntype: %i\n", str + sym[rel[i].info >> 8].name, rel[i].offset, rel[i].info & 255);
 	}
 	
 	if (!e) {
@@ -204,7 +274,7 @@ int8_t main(int32_t argc, int8_t** argv) {
 		sh[0].flags = 5;
 		sh[0].addr = 0;
 		sh[0].offset = 132;
-		sh[0].size = bi;
+		sh[0].size = bn;
 		sh[0].link = 0;
 		sh[0].info = 0;
 		sh[0].addralign = 0;
@@ -214,19 +284,19 @@ int8_t main(int32_t argc, int8_t** argv) {
 		sh[1].type = 3;
 		sh[1].flags = 6;
 		sh[1].addr = 0;
-		sh[1].offset = 132 + bi;
+		sh[1].offset = 132 + bn;
 		sh[1].size = 15;
 		sh[1].link = 0;
 		sh[1].info = 0;
 		sh[1].addralign = 0;
 		sh[1].entsize = 0;
 		
-		bi = elf_copy(bits, bi, ".shstrtab\0.text\0", 15);
+		bn = elf_copy(bits, bn, ".shstrtab\0.text\0", 15);
 		
 		FILE* f = fopen(argv[2], "w");
 		
-		uint8_t* elf = elf_write(argv[2], 1, &eh, 0, &sh, bits, bi);
-		uint64_t esz = eh.ehsize + (eh.phentsize * eh.phnum) + (eh.shentsize * eh.shnum) + bi;
+		uint8_t* elf = elf_write(argv[2], 1, &eh, 0, &sh, bits, bn);
+		uint64_t esz = eh.ehsize + (eh.phentsize * eh.phnum) + (eh.shentsize * eh.shnum) + bn;
 		
 		for (uint64_t i = 0; i < esz; i++) {
 			fprintf(f, "%c", elf[i]);
